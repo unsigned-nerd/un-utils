c5a53f63ec20e70bc5025efb4a0c8c025024f04b
unsigned_nerd <unsigned_nerd@member.fsf.org>
2020-05-06 22:58:39 +0700

    Add with-gensyms macro to un-utils.simple-syntax package

--- un-utils/simple-syntax.lisp
+++ un-utils/simple-syntax.lisp
@@ -3,7 +3,7 @@
 (defpackage :un-utils.simple-syntax
   (:use :common-lisp)
   (:export
-    #:for-each-$line-in #:$line #:print-line #:while))
+    #:for-each-$line-in #:$line #:print-line #:while #:with-gensyms))
 
 (in-package :un-utils.simple-syntax)
 
@@ -35,4 +35,20 @@
 (defmacro while (test &rest body)
   `(do ()
        ((not ,test))
+     ,@body))
+
+;
+; Used when writing a macro.  Use to create multiple local variables without having to call gensym
+; multiple times.
+;
+; Example:
+;
+;   (defmacro foo ()
+;     (with-gensyms (a b c)
+;       (setf a 2 b 3 c 4)
+;       `(+ ,a ,b ,c)))
+;
+(defmacro with-gensyms (syms &body body)
+  `(let ,(mapcar #'(lambda (s) `(,s (gensym)))
+                 syms)
      ,@body))
